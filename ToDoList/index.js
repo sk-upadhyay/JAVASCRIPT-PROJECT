1ca7e810dbd25ae8444fe9e872a4d966e3872566
sk-upadhyay <shivamws2003@gmail.com>
2024-01-04 21:48:49 +0530

    update

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,8 +1,17 @@
-const container = document.querySelector('#content');
-const button = document.querySelector('btn');
-const item = document.querySelector('items');
+const container = document.getElementById('content');
+const button = document.querySelector('.btn');
+const item = document.querySelector('.items');
 
 
-button.addEventListener('click',(e)=>{
-    item.innertext = container.value;
+button.addEventListener('click',() =>{
+    if(container.value!=""){
+        let list = document.createElement('li');
+        list.appendChild(document.createTextNode(container.value));
+        item.appendChild(list);
+        container.value="";
+
+        list.onclick = function(){
+            this.parentNode.remove;
+        }
+    }
 });
