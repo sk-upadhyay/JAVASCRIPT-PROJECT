ab71c7075bbea0d5e1e4f938c6a3e7fb97e30b1b
sk-upadhyay <shivamws2003@gmail.com>
2024-01-07 23:30:09 +0530

    modified:   ToDoList/index.js

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,8 +1,8 @@
 const container = document.getElementById('content');
 const button = document.querySelector('.btn');
 const item = document.querySelector('.items');
-
-
+const date = new Date();
+let year = date.getFullYear;
 button.addEventListener('click',() =>{
     if(container.value!=""){
         let list = document.createElement('li');
